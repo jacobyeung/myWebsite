3f173110509c2bcf0c2263ddd93e01c2166790f9
Jacob Yeung <jacyeung@cisco.com>
2019-06-14 15:33:40 -0700

    Changed background image to hoho

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("hello");
+import React from 'react';
+
+const styles = {
+    maxWidth: {
+        maxWidth: "6000px"
+    }
+}
+console.log("why are you here");
+
+class Background extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <img style={styles.maxWidth}
+                    src={"hoho.jpg"}/>
+            </div>
+        )
+    }
+}
